eadf1770b31e23ac665443f270ae5d4477eed740
Varphone Wong <varphone@qq.com>
2020-08-05 17:05:50 +0800

    Update implements
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- src/easy/mod.rs
+++ src/easy/mod.rs
@@ -1,2 +1,10 @@
-﻿pub mod mp4_writer;
+﻿use std::error::Error;
+
+/// Generic Result.
+pub type AVResult<T> = Result<T, Box<dyn Error>>;
+
 pub mod owned;
+pub use owned::*;
+
+pub mod writer;
+pub use writer::*;
--- src/easy/owned.rs
+++ src/easy/owned.rs
@@ -1,17 +1,18 @@
-use crate::ffi::{AVCodecID::*, *};
-use std::error::Error;
+use super::AVResult;
+use crate::ffi::*;
 use std::ffi::CString;
 use std::ops::{Deref, DerefMut};
 use std::path::Path;
 
-pub type AVResult<T> = Result<T, Box<dyn Error>>;
-
+/// Format context I/O mode.
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd)]
 pub enum AVFormatContextMode {
     Input,
     Output,
 }
 
+/// Format I/O context.
+#[derive(Debug)]
 pub struct AVFormatContextOwned {
     ptr: *mut AVFormatContext,
     mode: AVFormatContextMode,
@@ -19,7 +20,6 @@ pub struct AVFormatContextOwned {
 
 impl Drop for AVFormatContextOwned {
     fn drop(&mut self) {
-        println!("impl Drop for AVFormatContextOwned");
         match self.mode {
             AVFormatContextMode::Input => unsafe {
                 avformat_close_input(&mut self.ptr);
@@ -48,30 +48,34 @@ impl DerefMut for AVFormatContextOwned {
 }
 
 impl AVFormatContextOwned {
+    /// Wrap an exists AVFormatContext ptr.
     pub fn from_ptr(ptr: *mut AVFormatContext, mode: AVFormatContextMode) -> Self {
         Self { ptr, mode }
     }
 
-    pub fn with_output<N, P>(
+    /// Create a new AVFormatContext for output.
+    pub fn with_output<P>(
+        path: P,
+        format: Option<&str>,
         oformat: Option<&AVOutputFormat>,
-        format_name: N,
-        file_name: P,
     ) -> AVResult<Self>
     where
-        // O: AsRef<AVOutputFormat>,
-        N: AsRef<str>,
         P: AsRef<Path>,
     {
         unsafe {
             let mut ps = std::ptr::null_mut();
-            let format = CString::new(format_name.as_ref())?;
-            let path = CString::new(file_name.as_ref().as_os_str().to_str().unwrap()).unwrap();
+            let path = CString::new(path.as_ref().as_os_str().to_str().unwrap()).unwrap();
+            let mut format_ptr = std::ptr::null();
+            let cformat = CString::new(format.unwrap_or(""))?;
+            if format.is_some() {
+                format_ptr = cformat.as_ptr();
+            }
             let err = avformat_alloc_output_context2(
                 &mut ps,
                 oformat.map_or(std::ptr::null_mut(), |x| {
                     x as *const AVOutputFormat as *mut AVOutputFormat
                 }),
-                format.as_ptr(),
+                format_ptr,
                 path.as_ptr(),
             );
             if err < 0 {
@@ -92,12 +96,11 @@ impl AVFormatContextOwned {
         }
     }
 
+    /// Add a new stream to a media file.
     pub fn new_stream(&mut self, codec_id: AVCodecID) -> AVResult<AVStreamOwned> {
         unsafe {
+            // The codec is optional
             let codec = avcodec_find_encoder(codec_id);
-            if codec.is_null() {
-                return Err(format!("The encoder {:?} does not exists", codec_id).into());
-            }
             let stream = avformat_new_stream(self.ptr, codec);
             if stream.is_null() {
                 Err(format!("Failed to create new stream for {:?}", codec_id).into())
@@ -108,6 +111,7 @@ impl AVFormatContextOwned {
         }
     }
 
+    /// Allocate the stream private data and write the stream header to an output media file.
     pub fn write_header(&mut self) -> AVResult<()> {
         unsafe {
             let err = avformat_write_header(self.ptr, std::ptr::null_mut());
@@ -119,6 +123,7 @@ impl AVFormatContextOwned {
         }
     }
 
+    /// Write the stream trailer to an output media file and free the file private data.
     pub fn write_trailer(&mut self) -> AVResult<()> {
         unsafe {
             av_write_trailer(self.ptr);
@@ -126,6 +131,7 @@ impl AVFormatContextOwned {
         Ok(())
     }
 
+    /// Write a packet to an output media file ensuring correct interleaving.
     pub fn write_frame_interleaved(&mut self, packet: &mut AVPacket) -> AVResult<()> {
         unsafe {
             let err = av_interleaved_write_frame(self.ptr, packet);
@@ -138,6 +144,7 @@ impl AVFormatContextOwned {
     }
 }
 
+#[derive(Debug)]
 pub struct AVOutputFormatOwned {
     ptr: *mut AVOutputFormat,
 }
@@ -157,11 +164,13 @@ impl DerefMut for AVOutputFormatOwned {
 }
 
 impl AVOutputFormatOwned {
+    /// Wrap an exists AVOutputFormat ptr.
     pub fn from_ptr(ptr: *mut AVOutputFormat) -> Self {
         Self { ptr }
     }
 }
 
+#[derive(Debug)]
 pub struct AVStreamOwned {
     ptr: *mut AVStream,
 }
@@ -181,6 +190,7 @@ impl DerefMut for AVStreamOwned {
 }
 
 impl AVStreamOwned {
+    /// Wrap an exists AVStream ptr.
     pub fn from_ptr(ptr: *mut AVStream) -> Self {
         Self { ptr }
     }
